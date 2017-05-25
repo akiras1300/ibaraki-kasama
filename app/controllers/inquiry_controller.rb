3e2a5246931c11e04c31438887766de2f10a0209
akiras1300 <akiras130@gmail.com>
2017-05-25 21:37:10 +0900

    add setting

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -7,7 +7,7 @@ class InquiryController < ApplicationController
 
   def confirm
     # 入力値のチェック
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.valid?
       # OK。確認画面を表示
       render :action => 'confirm'
@@ -17,9 +17,17 @@ class InquiryController < ApplicationController
     end
   end
 
+  def inquiry_params
+    # submitしたデータのうち、Model作成に必要なものを
+    # permitの引数に指定する
+    params.require(:inquiry).permit(
+      :name, :email,:message
+    )
+  end
+
   def thanks
     # メール送信
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     InquiryMailer.received_email(@inquiry).deliver
 
     # 完了画面を表示
